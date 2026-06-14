7bfe237fefe6ca21cf5912b4e6593889bbc262ce
Aterban <mahtab.drive2022@gmail.com>
2026-06-14 22:30:17 +0330

    "service layer 3"

--- src/main/java/service/ManagerService.java
+++ src/main/java/service/ManagerService.java
@@ -6,9 +6,13 @@ public interface ManagerService {
 
     //manage services:
     // (add/delete specialist)
+
     // (add/delete service)
+
     // (edit service)
+
     //approve specialists
+
     //add/remove specialist
 
 
